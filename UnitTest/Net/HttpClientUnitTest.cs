444371d13b2fba46449ec160c6fa0924afc09bcb
kingcean <royaljinchen@msn.com>
2021-08-15 17:34:41 +0800

    Add HTTP client extension unit test.

---
+++ UnitTest/Net/HttpClientUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Trivial.Text;
+
+namespace Trivial.Net
+{
+    /// <summary>
+    /// HTTP URI unit test.
+    /// </summary>
+    [TestClass]
+    public class HttpClientUnitTest
+    {
+        /// <summary>
+        /// Tests query data.
+        /// </summary>
+        [TestMethod]
+        public void TestHttpClientExtensions()
+        {
+            using var content = HttpClientExtensions.CreateJsonContent((byte)0);
+            Assert.IsNotNull(content);
+            var mime = content.Headers.ContentType.ToString().YieldSplit(new List<string> { " " }).FirstOrDefault().YieldSplit(";", StringSplitOptions.RemoveEmptyEntries).FirstOrDefault();
+            Assert.AreEqual(Web.WebFormat.JsonMIME, mime);
+        }
+    }
+}
