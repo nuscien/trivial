2a34344763005e3be4fe7e62ff4989f775ad7fff
Kingcean <royaljinchen@msn.com>
2022-03-31 12:31:19 +0800

    Update comments of periodic table.

--- Chemistry/Chemistry/PeriodicTable.cs
+++ Chemistry/Chemistry/PeriodicTable.cs
@@ -14,592 +14,601 @@ namespace Trivial.Chemistry;
 public partial class ChemicalElement
 {
     /// <summary>
-    /// Gets Hydrogen.
+    /// Gets chemical element Hydrogen (Hydrogenium).
+    /// A colourless, odourless, tasteless, flammable gaseous substance that is the simplest member of the family of chemical elements.
     /// </summary>
     public static ChemicalElement H => Get(1);
 
     /// <summary>
-    /// Gets Helium.
+    /// Gets chemical element Helium.
+    /// The second lightest element, helium is a colourless, odourless, and tasteless gas that becomes liquid at −268.9 °C (−452 °F).
     /// </summary>
     public static ChemicalElement He => Get(2);
 
     /// <summary>
-    /// Gets Lithium.
+    /// Gets chemical element Lithium.
+    /// The lightest of the solid elements. The metal itself—which is soft, white, and lustrous—and several of its alloys and compounds are produced on an industrial scale.
     /// </summary>
     public static ChemicalElement Li => Get(3);
 
     /// <summary>
-    /// Gets Beryllium.
+    /// Gets chemical element Beryllium, formerly (until 1957AD) glucinium.
     /// </summary>
     public static ChemicalElement Be => Get(4);
 
     /// <summary>
-    /// Gets Boron.
+    /// Gets chemical element Boron.
     /// </summary>
     public static ChemicalElement B => Get(5);
 
     /// <summary>
-    /// Gets Carbon.
+    /// Gets chemical element Carbon (Carboneum).
     /// </summary>
     public static ChemicalElement C => Get(6);
 
     /// <summary>
-    /// Gets Nitrogen.
+    /// Gets chemical element Nitrogen (Nitrogenium).
     /// </summary>
     public static ChemicalElement N => Get(7);
 
     /// <summary>
-    /// Gets Oxygen.
+    /// Gets chemical element Oxygen (Oxygenium).
+    /// A colourless, odourless, tasteless gas essential to living organisms, being taken up by animals, which convert it to carbon dioxide.
     /// </summary>
     public static ChemicalElement O => Get(8);
 
     /// <summary>
-    /// Gets Fluorine.
+    /// Gets chemical element Fluorine (Fluorum).
     /// </summary>
     public static ChemicalElement F => Get(9);
 
     /// <summary>
-    /// Gets Neon.
+    /// Gets chemical element Neon.
     /// </summary>
     public static ChemicalElement Ne => Get(10);
 
     /// <summary>
-    /// Gets Sodium.
+    /// Gets chemical element Sodium (Natrium).
+    /// A very soft silvery-white metal.
     /// </summary>
     public static ChemicalElement Na => Get(11);
 
     /// <summary>
-    /// Gets Magnesium.
+    /// Gets chemical element Magnesium.
     /// </summary>
     public static ChemicalElement Mg => Get(12);
 
     /// <summary>
-    /// Gets Aluminium.
+    /// Gets chemical element Aluminium.
+    /// Also spelled aluminium, a lightweight silvery white metal.
     /// </summary>
     public static ChemicalElement Al => Get(13);
 
     /// <summary>
-    /// Gets Silicon.
+    /// Gets chemical element Silicon (Silicium).
     /// </summary>
     public static ChemicalElement Si => Get(14);
 
     /// <summary>
-    /// Gets Phosphorus.
+    /// Gets chemical element Phosphorus.
     /// </summary>
     public static ChemicalElement P => Get(15);
 
     /// <summary>
-    /// Gets Sulfur.
+    /// Gets chemical element Sulfur.
     /// </summary>
     public static ChemicalElement S => Get(16);
 
     /// <summary>
-    /// Gets Chlorine.
+    /// Gets chemical element Chlorine (Chlorum).
     /// </summary>
     public static ChemicalElement Cl => Get(17);
 
     /// <summary>
-    /// Gets Argon.
+    /// Gets chemical element Argon.
     /// </summary>
     public static ChemicalElement Ar => Get(18);
 
     /// <summary>
-    /// Gets Potassium.
+    /// Gets chemical element Potassium (Kalium).
     /// </summary>
     public static ChemicalElement K => Get(19);
 
     /// <summary>
-    /// Gets Calcium.
+    /// Gets chemical element Calcium.
     /// </summary>
     public static ChemicalElement Ca => Get(20);
 
     /// <summary>
-    /// Gets Scandium.
+    /// Gets chemical element Scandium.
     /// </summary>
     public static ChemicalElement Sc => Get(21);
 
     /// <summary>
-    /// Gets Titanium.
+    /// Gets chemical element Titanium.
     /// </summary>
     public static ChemicalElement Ti => Get(22);
 
     /// <summary>
-    /// Gets Vanadium.
+    /// Gets chemical element Vanadium.
     /// </summary>
     public static ChemicalElement V => Get(23);
 
     /// <summary>
-    /// Gets Chromium.
+    /// Gets chemical element Chromium.
     /// </summary>
     public static ChemicalElement Cr => Get(24);
 
     /// <summary>
-    /// Gets Manganese.
+    /// Gets chemical element Manganese (Manganum).
     /// </summary>
     public static ChemicalElement Mn => Get(25);
 
     /// <summary>
-    /// Gets Iron.
+    /// Gets chemical element Iron.
     /// </summary>
     public static ChemicalElement Fe => Get(26);
 
     /// <summary>
-    /// Gets Cobalt.
+    /// Gets chemical element Cobalt (Cobaltum).
     /// </summary>
     public static ChemicalElement Co => Get(27);
 
     /// <summary>
-    /// Gets Nickel.
+    /// Gets chemical element Nickel.
     /// </summary>
     public static ChemicalElement Ni => Get(28);
 
     /// <summary>
-    /// Gets Copper.
+    /// Gets chemical element Copper (Cuprum).
     /// </summary>
     public static ChemicalElement Cu => Get(29);
 
     /// <summary>
-    /// Gets Zinc.
+    /// Gets chemical element Zinc (Zincum).
     /// </summary>
     public static ChemicalElement Zn => Get(30);
 
     /// <summary>
-    /// Gets Gallium.
+    /// Gets chemical element Gallium.
     /// </summary>
     public static ChemicalElement Ga => Get(31);
 
     /// <summary>
-    /// Gets Germanium.
+    /// Gets chemical element Germanium.
     /// </summary>
     public static ChemicalElement Ge => Get(32);
 
     /// <summary>
-    /// Gets Arsenic.
+    /// Gets chemical element Arsenic (Arsenicum).
     /// </summary>
     public static ChemicalElement As => Get(33);
 
     /// <summary>
-    /// Gets Selenium.
+    /// Gets chemical element Selenium.
     /// </summary>
     public static ChemicalElement Se => Get(34);
 
     /// <summary>
-    /// Gets Bromine.
+    /// Gets chemical element Bromine (Bromum).
     /// </summary>
     public static ChemicalElement Br => Get(35);
 
     /// <summary>
-    /// Gets Krypton.
+    /// Gets chemical element Krypton.
     /// </summary>
     public static ChemicalElement Kr => Get(36);
 
     /// <summary>
-    /// Gets Rubidium.
+    /// Gets chemical element Rubidium.
     /// </summary>
     public static ChemicalElement Rb => Get(37);
 
     /// <summary>
-    /// Gets Strontium.
+    /// Gets chemical element Strontium.
     /// </summary>
     public static ChemicalElement Sr => Get(38);
 
     /// <summary>
-    /// Gets Yttrium.
+    /// Gets chemical element Yttrium.
     /// </summary>
     public static ChemicalElement Y => Get(39);
 
     /// <summary>
-    /// Gets Zirconium.
+    /// Gets chemical element Zirconium.
     /// </summary>
     public static ChemicalElement Zr => Get(40);
 
     /// <summary>
-    /// Gets Niobium.
+    /// Gets chemical element Niobium, formerly Columbium.
     /// </summary>
     public static ChemicalElement Nb => Get(41);
 
     /// <summary>
-    /// Gets Molybdenum.
+    /// Gets chemical element Molybdenum (Molybdaenum).
     /// </summary>
     public static ChemicalElement Mo => Get(42);
 
     /// <summary>
-    /// Gets Technetium.
+    /// Gets chemical element Technetium.
     /// </summary>
     public static ChemicalElement Tc => Get(43);
 
     /// <summary>
-    /// Gets Ruthenium.
+    /// Gets chemical element Ruthenium.
     /// </summary>
     public static ChemicalElement Ru => Get(44);
 
     /// <summary>
-    /// Gets Rhodium.
+    /// Gets chemical element Rhodium.
     /// </summary>
     public static ChemicalElement Rh => Get(45);
 
     /// <summary>
-    /// Gets Palladium.
+    /// Gets chemical element Palladium.
     /// </summary>
     public static ChemicalElement Pd => Get(46);
 
     /// <summary>
-    /// Gets Silver.
+    /// Gets chemical element Silver (Argentum).
+    /// A white lustrous metal valued for its decorative beauty and electrical conductivity.
     /// </summary>
     public static ChemicalElement Ag => Get(47);
 
     /// <summary>
-    /// Gets Cadmium.
+    /// Gets chemical element Cadmium.
     /// </summary>
     public static ChemicalElement Cd => Get(48);
 
     /// <summary>
-    /// Gets Indium.
+    /// Gets chemical element Indium.
     /// </summary>
     public static ChemicalElement In => Get(49);
 
     /// <summary>
-    /// Gets Tin.
+    /// Gets chemical element Tin.
     /// </summary>
     public static ChemicalElement Sn => Get(50);
 
     /// <summary>
-    /// Gets Antimony.
+    /// Gets chemical element Antimony.
     /// </summary>
     public static ChemicalElement Sb => Get(51);
 
     /// <summary>
-    /// Gets Tellurium.
+    /// Gets chemical element Tellurium.
     /// </summary>
     public static ChemicalElement Te => Get(52);
 
     /// <summary>
-    /// Gets Iodine.
+    /// Gets chemical element Iodine (Iodum).
     /// </summary>
     public static ChemicalElement I => Get(53);
 
     /// <summary>
-    /// Gets Xenon.
+    /// Gets chemical element Xenon.
     /// </summary>
     public static ChemicalElement Xe => Get(54);
 
     /// <summary>
-    /// Gets Caesium.
+    /// Gets chemical element Caesium.
     /// </summary>
     public static ChemicalElement Cs => Get(55);
 
     /// <summary>
-    /// Gets Barium.
+    /// Gets chemical element Barium.
     /// </summary>
     public static ChemicalElement Ba => Get(56);
 
     /// <summary>
-    /// Gets Lanthanum.
+    /// Gets chemical element Lanthanum.
     /// </summary>
     public static ChemicalElement La => Get(57);
 
     /// <summary>
-    /// Gets Cerium.
+    /// Gets chemical element Cerium.
     /// </summary>
     public static ChemicalElement Ce => Get(58);
 
     /// <summary>
-    /// Gets Praseodymium.
+    /// Gets chemical element Praseodymium.
     /// </summary>
     public static ChemicalElement Pr => Get(59);
 
     /// <summary>
-    /// Gets Neodymium.
+    /// Gets chemical element Neodymium.
     /// </summary>
     public static ChemicalElement Nd => Get(60);
 
     /// <summary>
-    /// Gets Promethium.
+    /// Gets chemical element Promethium.
     /// </summary>
     public static ChemicalElement Pm => Get(61);
 
     /// <summary>
-    /// Gets Samarium.
+    /// Gets chemical element Samarium.
     /// </summary>
     public static ChemicalElement Sm => Get(62);
 
     /// <summary>
-    /// Gets Europium.
+    /// Gets chemical element Europium.
     /// </summary>
     public static ChemicalElement Eu => Get(63);
 
     /// <summary>
-    /// Gets Gadolinium.
+    /// Gets chemical element Gadolinium.
     /// </summary>
     public static ChemicalElement Gd => Get(64);
 
     /// <summary>
-    /// Gets Terbium.
+    /// Gets chemical element Terbium.
     /// </summary>
     public static ChemicalElement Tb => Get(65);
 
     /// <summary>
-    /// Gets Dysprosium.
+    /// Gets chemical element Dysprosium.
     /// </summary>
     public static ChemicalElement Dy => Get(66);
 
     /// <summary>
-    /// Gets Holmium.
+    /// Gets chemical element Holmium.
     /// </summary>
     public static ChemicalElement Ho => Get(67);
 
     /// <summary>
-    /// Gets Erbium.
+    /// Gets chemical element Erbium.
     /// </summary>
     public static ChemicalElement Er => Get(68);
 
     /// <summary>
-    /// Gets Thulium.
+    /// Gets chemical element Thulium.
     /// </summary>
     public static ChemicalElement Tm => Get(69);
 
     /// <summary>
-    /// Gets Ytterbium.
+    /// Gets chemical element Ytterbium.
     /// </summary>
     public static ChemicalElement Yb => Get(70);
 
     /// <summary>
-    /// Gets Lutetium.
+    /// Gets chemical element Lutetium.
     /// </summary>
     public static ChemicalElement Lu => Get(71);
 
     /// <summary>
-    /// Gets Hafnium.
+    /// Gets chemical element Hafnium.
     /// </summary>
     public static ChemicalElement Hf => Get(72);
 
     /// <summary>
-    /// Gets Tantalum.
+    /// Gets chemical element Tantalum.
     /// </summary>
     public static ChemicalElement Ta => Get(73);
 
     /// <summary>
-    /// Gets Tungsten.
+    /// Gets chemical element Tungsten.
     /// </summary>
     public static ChemicalElement W => Get(74);
 
     /// <summary>
-    /// Gets Rhenium.
+    /// Gets chemical element Rhenium.
     /// </summary>
     public static ChemicalElement Re => Get(75);
 
     /// <summary>
-    /// Gets Osmium.
+    /// Gets chemical element Osmium.
     /// </summary>
     public static ChemicalElement Os => Get(76);
 
     /// <summary>
-    /// Gets Iridium.
+    /// Gets chemical element Iridium.
     /// </summary>
     public static ChemicalElement Ir => Get(77);
 
     /// <summary>
-    /// Gets Platinum.
+    /// Gets chemical element Platinum.
+    /// A very heavy, precious, silver-white metal, platinum is soft and ductile and has a high melting point and good resistance to corrosion and chemical attack.
     /// </summary>
     public static ChemicalElement Pt => Get(78);
 
     /// <summary>
-    /// Gets Gold.
+    /// Gets chemical element Gold (Aurum).
+    /// Gold has several qualities that have made it exceptionally valuable throughout history. It is attractive in colour and brightness, durable to the point of virtual indestructibility, highly malleable, and usually found in nature in a comparatively pure form.
     /// </summary>
     public static ChemicalElement Au => Get(79);
 
     /// <summary>
-    /// Gets Mercury.
+    /// Gets chemical element Mercury (Hydrargyrum).
     /// </summary>
     public static ChemicalElement Hg => Get(80);
 
     /// <summary>
-    /// Gets Thallium.
+    /// Gets chemical element Thallium.
     /// </summary>
     public static ChemicalElement Tl => Get(81);
 
     /// <summary>
-    /// Gets Lead.
+    /// Gets chemical element Lead (Plumbum).
     /// </summary>
     public static ChemicalElement Pb => Get(82);
 
     /// <summary>
-    /// Gets Bismuth.
+    /// Gets chemical element Bismuth (Bismuthum).
     /// </summary>
     public static ChemicalElement Bi => Get(83);
 
     /// <summary>
-    /// Gets Polonium.
+    /// Gets chemical element Polonium.
     /// </summary>
     public static ChemicalElement Po => Get(84);
 
     /// <summary>
-    /// Gets Astatine.
+    /// Gets chemical element Astatine (Astatium).
     /// </summary>
     public static ChemicalElement At => Get(85);
 
     /// <summary>
-    /// Gets Radon.
+    /// Gets chemical element Radon.
     /// </summary>
     public static ChemicalElement Rn => Get(86);
 
     /// <summary>
-    /// Gets Francium.
+    /// Gets chemical element Francium.
     /// </summary>
     public static ChemicalElement Fr => Get(87);
 
     /// <summary>
-    /// Gets Radium.
+    /// Gets chemical element Radium.
     /// </summary>
     public static ChemicalElement Ra => Get(88);
 
     /// <summary>
-    /// Gets Actinium.
+    /// Gets chemical element Actinium.
     /// </summary>
     public static ChemicalElement Ac => Get(89);
 
     /// <summary>
-    /// Gets Thorium.
+    /// Gets chemical element Thorium.
     /// </summary>
     public static ChemicalElement Th => Get(90);
 
     /// <summary>
-    /// Gets Protactinium.
+    /// Gets chemical element Protactinium.
     /// </summary>
     public static ChemicalElement Pa => Get(91);
 
     /// <summary>
-    /// Gets Uranium.
+    /// Gets chemical element Uranium.
     /// </summary>
     public static ChemicalElement U => Get(92);
 
     /// <summary>
-    /// Gets Neptunium.
+    /// Gets chemical element Neptunium.
     /// </summary>
     public static ChemicalElement Np => Get(93);
 
     /// <summary>
-    /// Gets Plutonium.
+    /// Gets chemical element Plutonium.
     /// </summary>
     public static ChemicalElement Pu => Get(94);
 
     /// <summary>
-    /// Gets Americium.
+    /// Gets chemical element Americium.
     /// </summary>
     public static ChemicalElement Am => Get(95);
 
     /// <summary>
-    /// Gets Curium.
+    /// Gets chemical element Curium.
     /// </summary>
     public static ChemicalElement Cm => Get(96);
 
     /// <summary>
-    /// Gets Berkelium.
+    /// Gets chemical element Berkelium.
     /// </summary>
     public static ChemicalElement Bk => Get(97);
 
     /// <summary>
-    /// Gets Californium.
+    /// Gets chemical element Californium.
     /// </summary>
     public static ChemicalElement Cf => Get(98);
 
     /// <summary>
-    /// Gets Einsteinium.
+    /// Gets chemical element Einsteinium.
     /// </summary>
     public static ChemicalElement Es => Get(99);
 
     /// <summary>
-    /// Gets Fermium.
+    /// Gets chemical element Fermium.
     /// </summary>
     public static ChemicalElement Fm => Get(100);
 
     /// <summary>
-    /// Gets Mendelevium.
+    /// Gets chemical element Mendelevium.
     /// </summary>
     public static ChemicalElement Md => Get(101);
 
     /// <summary>
-    /// Gets Nobelium.
+    /// Gets chemical element Nobelium.
     /// </summary>
     public static ChemicalElement No => Get(102);
 
     /// <summary>
-    /// Gets Lawrencium.
+    /// Gets chemical element Lawrencium.
     /// </summary>
     public static ChemicalElement Lr => Get(103);
 
     /// <summary>
-    /// Gets Rutherfordium.
+    /// Gets chemical element Rutherfordium.
     /// </summary>
     public static ChemicalElement Rf => Get(104);
 
     /// <summary>
-    /// Gets Dubnium.
+    /// Gets chemical element Dubnium.
     /// </summary>
     public static ChemicalElement Db => Get(105);
 
     /// <summary>
-    /// Gets Seaborgium.
+    /// Gets chemical element Seaborgium.
     /// </summary>
     public static ChemicalElement Sg => Get(106);
 
     /// <summary>
-    /// Gets Bohrium.
+    /// Gets chemical element Bohrium.
     /// </summary>
     public static ChemicalElement Bh => Get(107);
 
     /// <summary>
-    /// Gets Hassium.
+    /// Gets chemical element Hassium.
     /// </summary>
     public static ChemicalElement Hs => Get(108);
 
     /// <summary>
-    /// Gets Meitnerium.
+    /// Gets chemical element Meitnerium.
     /// </summary>
     public static ChemicalElement Mt => Get(109);
 
     /// <summary>
-    /// Gets Darmstadtium.
+    /// Gets chemical element Darmstadtium.
     /// </summary>
     public static ChemicalElement Ds => Get(110);
 
     /// <summary>
-    /// Gets Roentgenium.
+    /// Gets chemical element Roentgenium.
     /// </summary>
     public static ChemicalElement Rg => Get(111);
 
     /// <summary>
-    /// Gets Copernicium.
+    /// Gets chemical element Copernicium.
     /// </summary>
     public static ChemicalElement Cn => Get(112);
 
     /// <summary>
-    /// Gets Nihonium.
+    /// Gets chemical element Nihonium.
     /// </summary>
     public static ChemicalElement Nh => Get(113);
 
     /// <summary>
-    /// Gets Flerovium.
+    /// Gets chemical element Flerovium.
     /// </summary>
     public static ChemicalElement Fl => Get(114);
 
     /// <summary>
-    /// Gets Moscovium.
+    /// Gets chemical element Moscovium.
     /// </summary>
     public static ChemicalElement Mc => Get(115);
 
     /// <summary>
-    /// Gets Livermorium.
+    /// Gets chemical element Livermorium.
     /// </summary>
     public static ChemicalElement Lv => Get(116);
 
     /// <summary>
-    /// Gets Tennessine.
+    /// Gets chemical element Tennessine.
     /// </summary>
     public static ChemicalElement Ts => Get(117);
 
     /// <summary>
-    /// Gets Oganesson.
+    /// Gets chemical element Oganesson.
     /// </summary>
     public static ChemicalElement Og => Get(118);
 }
