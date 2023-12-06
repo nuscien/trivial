1be38d0d5552c666c70d83c6d14d80261bc2fdb8
Kingcean <royaljinchen@msn.com>
2023-12-06 15:28:16 +0800

    Clean up.

---
+++ Mime/Text/ChatMessageModification.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Trivial.Text;
+
+/// <summary>
+/// The modification kinds of the message.
+/// </summary>
+public enum ChatMessageModificationKinds : byte
+{
+    /// <summary>
+    /// The message is neve modified.
+    /// </summary>
+    Original = 0,
+
+    /// <summary>
+    /// The streaming message which means the message is transferring by continious updating.
+    /// </summary>
+    Streaming = 1,
+
+    /// <summary>
+    /// The message has been modified by sender.
+    /// </summary>
+    Modified = 2,
+
+    /// <summary>
+    /// The message has been modified and is open to update by others.
+    /// </summary>
+    Collaborative = 3,
+
+    /// <summary>
+    /// The message has been removed by sender.
+    /// </summary>
+    Removed = 5,
+
+    /// <summary>
+    /// The message is banned by system.
+    /// </summary>
+    Ban = 9,
+
+    /// <summary>
+    /// Others.
+    /// </summary>
+    Others = 15
+}
